c3c927442d69097084ad2aa3c3117412cb213c53
Dabeer <dabeerofficial@gmail.com>
2022-02-18 23:59:25 +0200

    basic stuff

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+  @Get()
+  getUser(): User[] {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string): User {
+    // TODO: auto parse ID
+    return this.usersService.findById(Number(id));
+  }
+
+  @Post()
+  createNewUser(@Body() body: CreateUserDto): User {
+    return this.usersService.createUser(body);
+  }
+}
